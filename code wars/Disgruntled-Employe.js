6232c0ed474105e6a38ef201b1912740efa6de2d
dieuaime-shami <shamidieuaime272@gmail.com>
2025-08-05 07:51:39 +0200

    Disgruntled-Employee

---
+++ code wars/Disgruntled-Employe.js
@@ -0,0 +1,17 @@
+// Create the function off, that receives the nth switch as argument n. The function should return an ascending 
+// array containing all of the switch numbers that remain off after Bob completes his revenge.
+
+// Example: (Input --> Output)
+// 1 --> [1]
+// 2 --> [1]
+// 4 --> [1, 4]
+
+function off(n) {
+    const result =[];
+    for (let i = 1; i <= n; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            result.push(i);
+        }
+    }
+    return result;
+}
